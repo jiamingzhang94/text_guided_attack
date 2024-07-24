e712d3fb6fd604cfcc7ea6e5698fd323239790c3
YeRookie <1282662051@qq.com>
2024-07-24 16:43:25 +0800

    修改数据和模型的缓存路径，修改输入图片路径，只调用test_dataloader

--- lavis/retriveval.py
+++ lavis/retriveval.py
@@ -1,5 +1,7 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]= "1"
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# 设定下载的模型和预训练模型的缓存路径
 os.environ['TORCH_HOME'] = '/new_data/yifei2/junhong/AttackVLM-main/model/blip-cache'
 import argparse
 import random
@@ -26,16 +28,18 @@ from lavis.runners.runner_base import RunnerBase
 from lavis.tasks import *
 from lavis.common.registry import registry
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Training")
 
-    parser.add_argument("--cfg-path", default="ret_coco_eval.yaml",help="path to configuration file.")
+    parser.add_argument("--cfg-path", default="ret_coco_eval.yaml", help="path to configuration file.")
+    parser.add_argument("--cache_path", default="/new_data/yifei2/junhong/dataset", help="path to dataset cache")
     parser.add_argument(
         "--options",
         nargs="+",
         help="override some settings in the used config, the key-value pair "
-        "in xxx=yyy format will be merged into config file (deprecate), "
-        "change to --cfg-options instead.",
+             "in xxx=yyy format will be merged into config file (deprecate), "
+             "change to --cfg-options instead.",
     )
 
     args = parser.parse_args()
@@ -61,12 +65,12 @@ def main():
     # os.environ["NCCL_BLOCKING_WAIT"] = "1"
 
     # set before init_distributed_mode() to ensure the same job_id shared across all ranks.
-
-    # 修改缓存路径
-    registry.mapping["paths"]["cache_root"] = "/new_data/yifei2/junhong/dataset"
+    args = parse_args()
+    # 修改缓存路径，默认的缓存路径没有访问权限，修改缓存到指定位置
+    registry.mapping["paths"]["cache_root"] = args.cache_path
     job_id = now()
 
-    cfg = Config(parse_args())
+    cfg = Config(args)
 
     init_distributed_mode(cfg.run_cfg)
 
@@ -81,9 +85,18 @@ def main():
     datasets = task.build_datasets(cfg)
     model = task.build_model(cfg)
 
+    task_key = list(datasets.keys())[0]
+    new_datasets = {task_key: {"test": datasets[task_key]["test"]}}  # 只用测试集当中的五千个样本
     runner = RunnerBase(
-        cfg=cfg, job_id=job_id, task=task, model=model, datasets=datasets
+        cfg=cfg, job_id=job_id, task=task, model=model, datasets=new_datasets
     )
+
+    # 默认的保存路径为registry.get_path("library_root")+output_dir/evaluate.txt 此处修改为配置文件中路径
+    output_dir =os.path.join(cfg.run_cfg["output_dir"], job_id)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    registry.mapping["paths"]["output_dir"] = output_dir
+
     runner.evaluate(skip_reload=True)
 
 
