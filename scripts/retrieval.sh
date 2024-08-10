26968daa509653aa9a5b45f7ace0e86e732753c9
YeRookie <1282662051@qq.com>
2024-08-10 12:32:23 +0800

    修改图像路径

--- scripts/retrieval.sh
+++ scripts/retrieval.sh
@@ -9,13 +9,27 @@ DATA=/home/dycpu6_8tssd1/jmzhang/datasets/
 
 datasets=("coco" "flickr30k")
 targets=("clip" "blip" "albef")
+image_path=("/YOUR/COCO/PATH","YOUR/FLICKER/PATH")
 
+# 使用索引变量i来遍历两个数组
 for t in "${targets[@]}"; do
-    for d in "${datasets[@]}"; do
+    for ((i=0; i<${#datasets[@]}; i++)); do
+        d="${datasets[$i]}"
+        image="${image_path[$i]}"
         python retrieval.py \
         --cache_path ${DATA} \
-        --cfg_path ./lavis_tool/${t}/ret_${d}_eval.yaml
+        --cfg_path ./lavis_tool/${t}/ret_${d}_eval.yaml \
+        --image_path "${image}"
     done
 done
+
+#for t in "${targets[@]}"; do
+#    for d in "${datasets[@]}"; do
+#        python retrieval.py \
+#        --cache_path ${DATA} \
+#        --cfg_path ./lavis_tool/${t}/ret_${d}_eval.yaml \
+#        --image_path ${IMAGE_PATH}
+#    done
+#done
 #        --adv_training \
 #                 > logs/retrieval/eval.log 2>&1
