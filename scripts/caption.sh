5df3e2ab8f1aedc234323535a92cf6aad0bf60b6
YeRookie <1282662051@qq.com>
2024-09-01 23:49:06 +0800

    caption python和shell脚本

---
+++ scripts/caption.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 定义变量
+DATA="/home/dycpu6_8tssd1/jmzhang/datasets/"
+DATASETS=("coco_caption" "nocaps")
+METHODS=("early" "later" "scratch")
+LOSSES=("Con" "Cosine")
+
+# 循环执行脚本
+for L in "${LOSSES[@]}"; do
+    for M in "${METHODS[@]}"; do
+        for DATASET in "${DATASETS[@]}"; do
+            echo "---------------------------------------------------"
+            echo "Dataset: $DATASET, Image Path: ${DATASET}/${M}_${L} "
+            COMMAND="python caption.py --cache_path $DATA --image_path outputs/${DATASET}/${M}_${L} --json_path outputs/${DATASET}_${M}_${L}.json"
+            if [ "$DATASET" = "coco_caption" ]; then
+                COMMAND+=" --cfg_path lavis_tool/blip/caption_coco_eval.yaml"
+            elif [ "$DATASET" = "nocaps" ]; then
+                COMMAND+=" --cfg_path lavis_tool/blip/caption_nocaps_eval.yaml"
+            fi
+            eval $COMMAND
+        done
+    done
+done
---
+++ scripts/caption_shell.py
@@ -0,0 +1,26 @@
+import os
+
+# 定义变量
+DATA = "/home/dycpu6_8tssd1/jmzhang/datasets/"
+datasets = [ "coco_caption","nocaps"]
+methods = ["early", "later", "scratch"]
+losses = ["Con", "Cosine"]
+
+# 循环执行脚本
+for l in losses:
+    for m in methods:
+        for i in range(len(datasets)):
+            print("---------------------------------------------------")
+            print(f"Dataset: {datasets[i]}, Image Path: {datasets[i]}/{m}_{l} ")
+
+            command = (
+                f"python caption.py "
+                f"--cache_path {DATA} "
+                f"--image_path outputs/{datasets[i]}/{m}_{l} "
+                f"--json_path outputs/{datasets[i]}_{m}_{l}.json "
+            )
+            if datasets[i] == "coco_caption":
+                command+=f" --cfg_path lavis_tool/blip/caption_coco_eval.yaml "#没有需要更换的backbone
+            elif datasets[i]== "nocaps":
+                command+=f" --cfg_path lavis_tool/blip/caption_nocaps_eval.yaml "
+            os.system(command)
